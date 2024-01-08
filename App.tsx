c05dc110c02268a26259f27975b7d42d0b0b00b0
Henrique Rodrigues <henrique_krf@outlook.com>
2024-01-07 21:05:53 -0300

    componentes e propriedades

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import { Aluno } from './src/components/alunos';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [nome, setNome] = useState('');
+  const [idade, setIdade] = useState<string | number>('');
+
+  function handleChangeName(nome: string, idade: number | string) {
+    setNome(nome);
+    setIdade(idade);
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Teste</Text>
-
-      <Aluno nome="Henrique" />
+      <Pressable onPress={() => handleChangeName("Ana Caroline", 19)}>
+        <Text>Mudar Nome</Text>
+      </Pressable>
+      {nome !== '' && <Aluno nome={nome} idade={idade} />}
     </View>
   );
 }
--- src/components/alunos.tsx
+++ src/components/alunos.tsx
@@ -1,11 +1,26 @@
 import { View, Text, StyleSheet } from 'react-native';
 
-export function Aluno({ nome }: { nome: string }) {
+interface AlunoProps {
+    nome: string;
+    idade: string | number;
+}
+
+export function Aluno({ nome, idade }: AlunoProps) {
     return (
-        <View>
-            <Text>Bem vindo {nome}</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Bem vindo {nome}</Text>
+            <Text style={styles.text}>Idade: {idade}</Text>
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        padding: 14,
+        backgroundColor: '#121212',
+        borderRadius: 8,
+    },
+    text: {
+        color: '#FFF'
+    }
+});
